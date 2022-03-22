bc573ac1204ecc477f41d9cde1c988504f0382d1
Tyler Long <tyler4long@gmail.com>
2022-03-22 15:12:18 -0700

    Hello Monaco

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hell world');
+import * as monaco from 'monaco-editor';
+
+import './index.css';
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+
+monaco.editor.create(container, {
+  value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+  language: 'javascript',
+});
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,7 @@
 /* eslint-disable node/no-unpublished-import */
 import {Configuration} from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin';
 
 const config: Configuration = {
   mode: 'development',
@@ -11,7 +12,20 @@ const config: Configuration = {
     new HtmlWebpackPlugin({
       title: 'Monaco Editor ESLint Demo',
     }),
+    new MonacoWebpackPlugin(),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.ttf$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
 
 export default [config];
