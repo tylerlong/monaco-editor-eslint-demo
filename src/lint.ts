7adfda0725185bf1e08606558a75b67181f928e5
Tyler Long <tyler4long@gmail.com>
2022-03-22 21:27:04 -0700

    Refactor code

---
+++ src/lint.ts
@@ -0,0 +1,38 @@
+import { Linter } from 'eslint';
+import { parseForESLint } from '@typescript-eslint/parser';
+import eslintPlugin from '@typescript-eslint/eslint-plugin';
+import nodePlugin from 'eslint-plugin-node';
+import eqeqeqFixPlugin from 'eslint-plugin-eqeqeq-fix';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+import rules from './rules.json';
+
+const linter = new Linter();
+
+const temp: { [name: string]: any } = {};
+for (const key of Object.keys(eslintPlugin.rules)) {
+  temp[`@typescript-eslint/${key}`] = eslintPlugin.rules[key];
+}
+for (const key of Object.keys(nodePlugin.rules)) {
+  temp[`node/${key}`] = nodePlugin.rules[key];
+}
+for (const key of Object.keys(eqeqeqFixPlugin.rules)) {
+  temp[`eqeqeq-fix/${key}`] = eqeqeqFixPlugin.rules[key];
+}
+for (const key of Object.keys(prettierPlugin.rules)) {
+  temp[`prettier/${key}`] = prettierPlugin.rules[key];
+}
+linter.defineRules(temp);
+linter.defineParser('@typescript-eslint/parser', {
+  parseForESLint: parseForESLint as any,
+});
+
+const lint = async (code: string) => {
+  const result = linter.verify(code, {
+    rules,
+    parser: '@typescript-eslint/parser',
+  } as any);
+  return result;
+};
+
+export default lint;
--- src/test.ts
+++ src/test.ts
@@ -1,38 +1,4 @@
-import { Linter } from 'eslint';
-import { parseForESLint } from '@typescript-eslint/parser';
-import eslintPlugin from '@typescript-eslint/eslint-plugin';
-import nodePlugin from 'eslint-plugin-node';
-import eqeqeqFixPlugin from 'eslint-plugin-eqeqeq-fix';
-import prettierPlugin from 'eslint-plugin-prettier';
+import lint from './lint';
 
-import rules from './rules.json';
-
-const linter = new Linter();
-
-const main = async () => {
-  const temp: { [name: string]: any } = {};
-  for (const key of Object.keys(eslintPlugin.rules)) {
-    temp[`@typescript-eslint/${key}`] = eslintPlugin.rules[key];
-  }
-  for (const key of Object.keys(nodePlugin.rules)) {
-    temp[`node/${key}`] = nodePlugin.rules[key];
-  }
-  for (const key of Object.keys(eqeqeqFixPlugin.rules)) {
-    temp[`eqeqeq-fix/${key}`] = eqeqeqFixPlugin.rules[key];
-  }
-  for (const key of Object.keys(prettierPlugin.rules)) {
-    temp[`prettier/${key}`] = prettierPlugin.rules[key];
-  }
-  linter.defineRules(temp);
-  linter.defineParser('@typescript-eslint/parser', {
-    parseForESLint: parseForESLint as any,
-  });
-
-  const result = linter.verify('var a = 1', {
-    rules,
-    parser: '@typescript-eslint/parser',
-  } as any);
-  console.log(result);
-};
-
-main();
+const result = lint('var a = 1');
+console.log(result);
