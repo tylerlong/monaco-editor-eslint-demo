eb67106feed5729d0ae1cf650e22e9a871a8fb4e
Tyler Long <tyler4long@gmail.com>
2022-03-22 18:22:54 -0700

    Build it

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,12 +1,16 @@
 /* eslint-disable node/no-unpublished-import */
-import {Configuration} from 'webpack';
+import { Configuration } from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin';
+import path from 'path';
 
 const config: Configuration = {
   mode: 'development',
   entry: {
     index: './src/index.ts',
+  },
+  output: {
+    path: path.join(__dirname, 'docs'),
   },
   plugins: [
     new HtmlWebpackPlugin({
